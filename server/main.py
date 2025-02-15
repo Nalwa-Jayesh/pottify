3c2fe795a0ec90c3af6c46036504dd3ee4b14f6c
Nalwa-Jayesh <64242951+Nalwa-Jayesh@users.noreply.github.com>
2025-02-15 21:29:52 +0530

    Added root endpoint

--- server/main.py
+++ server/main.py
@@ -6,6 +6,10 @@ from routes import auth, song
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI Backend is Running!"}
+
 app.include_router(auth.router, prefix='/auth')
 app.include_router(song.router, prefix='/song')
 Base.metadata.create_all(engine)
