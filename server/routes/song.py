f537e22f20267cfde8676bbc2b837f5731452520
Nalwa-Jayesh <64242951+Nalwa-Jayesh@users.noreply.github.com>
2025-02-18 00:48:16 +0530

    Songs endpoint complete with cloudinary

--- server/routes/song.py
+++ server/routes/song.py
@@ -17,7 +17,7 @@ load_dotenv()
 router = APIRouter()
 
 cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_NAME"),
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
     api_key=os.getenv("CLOUDINARY_API_KEY"),
     api_secret=os.getenv("CLOUDINARY_API_SECRET"),
     secure=True
